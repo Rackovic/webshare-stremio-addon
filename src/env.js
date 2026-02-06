7fa56dce78567058028b2cc87b637f7c6ae1bc56
Rackovic <133105846+Rackovic@users.noreply.github.com>
2026-02-06 13:03:54 +0100

    Delete src/env.js

--- src/env.js
+++
@@ -1,9 +0,0 @@
-const host =
-  process.argv.includes("--dev") == 1
-    ? "localhost:61613"
-    
-const url =
-  process.argv.includes("--dev") == 1
-    ? "http://localhost:61613/"
-
-module.exports = { host, url };
