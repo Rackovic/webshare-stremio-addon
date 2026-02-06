b764885037bc414c57023b47631f307086057313
Rackovic <133105846+Rackovic@users.noreply.github.com>
2026-02-06 13:22:40 +0100

    Update env.js

--- src/env.js
+++ src/env.js
@@ -1,11 +1,11 @@
 const host =
   process.argv.includes("--dev") == 1
     ? "localhost:61613"
-    : "20317bf4c6c6-webshare-stremio-addon.baby-beamup.club";
+    : "satlink-cinema-stremio-addon.onrender.com"; // TU MUSÍ BYŤ TVOJA RENDER ADRESA
 
 const url =
   process.argv.includes("--dev") == 1
     ? "http://localhost:61613/"
-    : "https://20317bf4c6c6-webshare-stremio-addon.baby-beamup.club/";
+    : "https://satlink-cinema-stremio-addon.onrender.com/"; // AJ TU
 
 module.exports = { host, url };
